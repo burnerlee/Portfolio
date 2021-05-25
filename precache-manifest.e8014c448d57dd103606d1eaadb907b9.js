584dbe3dee020b1c307fd3728480417ffd5dae9e
Aviral Jain <avi.aviral140@gmail.com>
2021-05-25 06:44:36 +0530

    Updates

---
+++ precache-manifest.e8014c448d57dd103606d1eaadb907b9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3399c087256a9b2345a",
+    "url": "/static/css/main.60db6ad6.chunk.css"
+  },
+  {
+    "revision": "c3399c087256a9b2345a",
+    "url": "/static/js/main.3f7a8822.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cf598f12d51b13edb0e0",
+    "url": "/static/js/2.1c9c5d6e.chunk.js"
+  },
+  {
+    "revision": "37947d181fe865614dadf0b88dbb7f0b",
+    "url": "/static/media/gsoc.37947d18.png"
+  },
+  {
+    "revision": "c68fadbe0fe795f5e6f636fa7dd58bbe",
+    "url": "/static/media/sdslabsdark.c68fadbe.jpeg"
+  },
+  {
+    "revision": "b4e6aa85e8fcc616e2347ecc1feb709e",
+    "url": "/static/media/marigold.b4e6aa85.jpg"
+  },
+  {
+    "revision": "592d5bcaf64567301a80f0072f343919",
+    "url": "/static/media/iitr.592d5bca.jpg"
+  },
+  {
+    "revision": "d6b08844178e584d91e7b07d3b1707bb",
+    "url": "/static/media/chinar.d6b08844.png"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "03cfd7bcf39ceb6e0a84beda5d7b9a0b",
+    "url": "/static/media/Agustina.03cfd7bc.otf"
+  },
+  {
+    "revision": "a07a9ae74e86cc057643f555aba0c723",
+    "url": "/index.html"
+  }
+];
